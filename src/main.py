3271d0fa658e99c05555afa0deab5968d16b582e
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-01 01:53:10 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+# John Melody Me
+# Data Get from https://www.kaggle.com/sudalairajkumar/novel-corona-virus-2019-dataset
 import pandas
 from pandas import read_csv
+from datetime import datetime, date
 
+currentDateTime = datetime.now()
 statistic_data = "data\covid_19_data.csv"
-header = ["No", "Date", "Province", "Country", "Update", "Confirmed", "Deaths", "Recovered"]
-dataset = read_csv(statistic_data, names=header)
+names = ["No", "Date", "Province", "Country", "Update", "Confirmed", "Deaths", "Recovered"]
+dataset = read_csv(statistic_data, names=names)
+# print(dataset.shape) #(10359, 8) (columns, row)
+# print(dataset.head(50))
+print(currentDateTime, "\n" ,"World Wide <data> \n\n ", dataset.describe)
