a223dcd994d7ffaf63a30da70575ccc1d2492fb2
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-08 02:13:17 +0800

    uodated

---
+++ covid19Forecast.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+Copyright = """
+                  Copyright 2020 © John Melody Me
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+                  http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+      @Author : John Melody Me
+      @Copyright: John Melody Me & Tan Sin Dee © Copyright 2020
+      @INPIREDBYGF: Cindy Tan Sin Dee <3
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+import plotly.graph_objects as go
+# from fbprophet import Prophet
+import pycountry
+import plotly.express as px
+from collections import namedtuple
